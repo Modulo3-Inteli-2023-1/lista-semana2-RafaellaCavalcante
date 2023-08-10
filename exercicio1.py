ce06a593e51c4d9f0bcf7b02e446212911f217f5
Babbly-eng <Rafaella.Cavalcante@sou.inteli.edu.br>
2023-08-10 14:13:18 -0300

    exercicio-semana-dois

--- exercicio1.py
+++ exercicio1.py
@@ -6,10 +6,10 @@
 
 # Crie a função que será avaliada no exercício aqui
 
-
-
-
-
+def conta_palavras_unicas(frutas):
+    lista = frutas.split()
+    conjunto = set(lista)
+    return len(conjunto)
 
 
 # Teste a sua função aqui (caso ache necessário)
--- exercicio2.py
+++ exercicio2.py
@@ -6,7 +6,12 @@
 
 # Crie a função que será avaliada no exercício aqui
 
-
+def is_anagram(p1, p2):
+    if len(p1) == len(p2):
+        ordem1 = sorted(p1)
+        ordem2 = sorted(p2)
+        return ordem1 == ordem2
+    return False
 
 
 
